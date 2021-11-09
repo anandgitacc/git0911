18392c7b0e6f8d43cfbaa9306bc0cd85c7cba718
anandgitacc <anand.akmail@gmail.com>
2021-11-09 15:07:05 +0530

    testone

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package org.employee;
+// class
+public class EmployeeDetails {
+	// method 1
+	private void empName() {
+		
+		System.out.println("Employee name is : Anand");
+	}
+	// method 2
+	private void empMob() {
+		
+		System.out.println("Employee mobile number is : 893927886");
+	}
+	// method 3
+	private void empAdd() {
+
+	}
+	// main method
+	public static void main(String[] args) {
+		// object
+		EmployeeDetails e = new EmployeeDetails();
+		// method call
+		e.empName();
+		e.empMob();
+		
+	}
+
+}
